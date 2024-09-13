bbad359171ea3058c6dcfba92ef487f2e2daec78
Johan <105075095+XanthusCode@users.noreply.github.com.>
2024-09-13 17:41:07 -0500

    feat: :white_check_mark: Cambios Casi Finales

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-const images = document.querySelectorAll('.image');
+  // Obtener el número de visitas desde el almacenamiento local
+  let visitCount = localStorage.getItem('visitCount');
+  if (visitCount === null) {
+      visitCount = 0;
+  }
+  visitCount++;
+  localStorage.setItem('visitCount', visitCount);
+
+  // Mostrar el contador en la página
+  document.getElementById('visit-count').textContent = 'Number of visits: ' + visitCount;
